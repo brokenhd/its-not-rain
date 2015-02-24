906b7c761f23ad7fecf3db98990c66c029728570
Marco <makrokorg@gmail.com>
2015-02-23 20:29:46 -0800

    movement script

---
+++ Scripts/Characters/movement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+
+	
+	
+	void Update () 
+	{
+
+
+
+		//Gets 
+		if (Input.GetButton ("Vertical")) {
+			if (Input.GetAxis ("Vertical") > 0) {
+				transform.Translate (Vector3.up * Time.deltaTime, Space.World);
+			} else {
+				transform.Translate (-Vector3.up * Time.deltaTime, Space.World);
+			}
+		}
+		if (Input.GetButton ("Horizontal")) {
+			if (Input.GetAxis ("Horizontal") > 0) {
+					transform.Translate (Vector3.right * Time.deltaTime, Space.World);
+			} else {
+					transform.Translate (-Vector3.right * Time.deltaTime, Space.World);
+			}
+		}
+	}
+
+
+}
