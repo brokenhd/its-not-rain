42a30ca61c10e3b61a8e0332f5bab89bb960b69a
Marco <makrokorg@gmail.com>
2015-03-02 21:14:16 -0800

    Started Inventory Script

---
+++ Scripts/Characters/Item.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+
+	public string firstAbilityName = "fireball";
+	public int firstAbilitycurrentExp = 5;
+	public int firstAbilityexperienceRemainder = 95;
+	public int firstAbilitycurrentLevel = 5;
+	public int firstAbilityPower = 95;
+	
+	
+	public string secondAbilityName = "waterball";
+	public int secondAbilitycurrentExp = 8;
+	public int secondAbilityexperienceRemainder = 92;
+	public int secondAbilitycurrentLevel = 9;
+	public int secondAbilityPower = 99999999;
+
+}
