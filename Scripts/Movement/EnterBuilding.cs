41be89552f46adcbb3c9b8bcc349ecf4fa58565a
Michael Starks <michaelastarks@gmail.com>
2015-03-16 19:01:12 -0700

    new stuff

---
+++ Scripts/Movement/EnterBuilding.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnterBuilding : MonoBehaviour {
+
+	public string areaName;
+	
+	void Update () {
+
+
+	}
+
+	void OnTriggerStay(Collider player) {	
+		GUI.Label (new Rect(10,10,100,20), "Push E to enter");
+		Debug.Log("Enter building");
+
+		if (Input.GetButton("Use")) {
+			Application.LoadLevel (areaName);
+		}
+	}
+}
